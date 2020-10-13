63ad17d2e6c176cbbe253162a1afa6fe3c033702
DESKTOP-MUKPOOP\lrdsl <lrdslerma@gmail.com>
2020-10-13 18:59:34 -0500

    Modelo Carrera

---
+++ ulsaservicios/database/migrations/2020_10_13_234848_create_carreras_table.php
@@ -0,0 +1,34 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+class CreateCarrerasTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('carreras', function (Blueprint $table) {
+            $table->bigIncrements('id');
+            $table->string('nombre_carrera',80);
+            $table->integer('coordinacion_id');
+            $table->integer('activo')->default(1);
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('carreras');
+    }
+}
