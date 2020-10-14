f96957b2ca3ff0fd18896cb5c03f0633e83d575e
Roberto Villegas <hola@robertovillegas.com>
2020-10-13 19:21:44 -0500

    Corrección en migración

--- ulsaservicios/database/migrations/2020_10_14_001309_create_cajas_table.php
+++ ulsaservicios/database/migrations/2020_10_14_001309_create_cajas_table.php
@@ -14,8 +14,6 @@ class CreateCajasTable extends Migration
     public function up()
     {
         Schema::create('cajas', function (Blueprint $table) {
-            $table->id();
-            $table->timestamps();
             $table->bigIncrements('id');
             $table->timestamps();
             $table->integer('tramite_id');
