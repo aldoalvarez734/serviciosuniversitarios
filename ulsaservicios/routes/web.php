5a02aeb23d9f2226f11927603315fa4110f1a6f5
Roberto <hola@robertovillegas.com>
2020-10-19 23:18:47 -0500

    Commit simbolico

--- ulsaservicios/routes/web.php
+++ ulsaservicios/routes/web.php
@@ -11,13 +11,13 @@ use Illuminate\Support\Facades\Route;
 | routes are loaded by the RouteServiceProvider within a group which
 | contains the "web" middleware group. Now create something great!
 |
-*/
+ */
 
 Route::get('/', function () {
     return view('welcome');
 });
 
-Route::get('/alumnos','AlumnosController@index');
-Route::get('/nuevo','AlumnosController@create');
-Route::get('/carreras','CarrerasControler@index');
-Route::get('/caja','CajaControler@index');
+Route::get('/alumnos', 'AlumnosController@index');
+Route::get('/nuevo', 'AlumnosController@create');
+Route::get('/carreras', 'CarrerasControler@index');
+Route::get('/caja', 'CajaControler@index');
