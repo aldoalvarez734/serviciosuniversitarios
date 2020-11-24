91b3d258a51bdfce5110c5a26cc44a0535ec7ffb
NatanaeLR <69933480+NatanaeLR@users.noreply.github.com>
2020-11-24 17:45:42 -0600

    Index (huecos) y botones de Crear Nuevo de Documentos,Oficina, Boleta de Calificaciones, Encargado,Evaluaciones del Servicio Becario,Estatus Alumno

---
+++ ulsaservicios/resources/views/Evaluaciones_ServicioBecario/index.blade.php
@@ -0,0 +1,22 @@
+@extends('layouts.app')
+
+@section('content')
+<div class="container">
+    <div class="row justify-content-center">
+        <div class="col-md-12">
+            <div class="card">
+                <div class="card-header">LISTADO DE OFICINAS</div>
+                <div class="container">
+               <a href="/nuevo/evaluacion_del_servicio_becarios" class="btn btn-success">Nuevo</a>
+              </div>
+
+                <div class="card-body">
+                  <H1>
+                      AQUI ESTA LA VISTA DE LAS OFICINAS
+                  </H1>
+                </div>
+            </div>
+        </div>
+    </div>
+</div>
+@endsection
